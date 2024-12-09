e38ce50209bbe2f666798c0912c58dae73f1d78d
micto <maksimka.vlasov.2006@gmail.com>
2024-12-09 22:37:36 +1000

    upd

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import class_enemy as ce
 import random as rd
 import menu as mn
 
+########################
+
 p_name = input('введите имя')
 p = ch.Player(p_name)
 
---
+++ menu.py
@@ -0,0 +1,6 @@
+class main_menu():
+    print('1. Начать игру')
+    print('2. Продолжить игру')
+    print('3. Выход')
+    opt = int(input('> '))
+    
