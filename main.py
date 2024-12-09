0e2946a146778ab9dde90fa7cacba1a8699252c8
micto <maksimka.vlasov.2006@gmail.com>
2024-12-09 22:05:33 +1000

    upd

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import config as con
 import class_char as ch
 import class_enemy as ce
-import random as r
+import random as rd
+import menu as mn
 
 p_name = input('введите имя')
 p = ch.Player(p_name)
